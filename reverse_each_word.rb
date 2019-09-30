62179149dad3a62b70a5b6b7cccc853d8cd58c99
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-09-30 13:25:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(str)
-  str_array = %w[str]
+  str_array = str.split("")
   reversed_array = str_array.each{|word| word.reverse}
   reversed_str = reversed_array.join("")
   reversed_str
